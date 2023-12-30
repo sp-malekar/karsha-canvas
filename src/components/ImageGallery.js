bd46f97ce73c980e2723080ea26d73db4e8457aa
sp-malekar <shiva.malekar06@gmail.com>
2023-12-31 02:33:44 +0530

    :sparkles:added more static images

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,25 +1,103 @@
 import React from 'react';
 import ImageGrid from './ImageGrid';
 import logo from '../images/logo.png'
-import child from '../images/child.jpg'
+import Forest from '../images/Forest.jpg'
+import Monument from '../images/Monument.jpg'
+import Mountains from '../images/Mountains.jpg'
+import Krishna from '../images/Krishna.jpg'
+import Ganesh from '../images/Ganesh.jpg'
+import Ranges from '../images/Ranges.jpg'
+import Vally from '../images/Vally.jpg'
+import Vikendi from '../images/Vikendi.jpg'
+import Temple from '../images/Temple.jpg'
+import City from '../images/City.jpg'
+import Himalayas from '../images/Himalayas.jpg'
+import japan from '../images/japan.jpg'
+import SunFlowers from '../images/SunFlowers.jpg'
+import Shiva from '../images/Shiva.jpg'
+
 
 const ImageGallery = () => {
   const images = [
     {
-      src: logo,
+      src: Shiva,
       children: [
-        { src: child },
-        { src: logo },
+        { src: Temple },
+        { src: Monument },
       ],
     },
     {
-      src: logo,
+      src: Mountains,
       children: [
-        { src: child },
+        { src: City },
+        { src: Forest },
+        // ... more child images
+      ],
+    },
+    {
+      src: Krishna,
+      children: [
+        { src: Temple },
         { src: logo },
         // ... more child images
       ],
     },
+    {
+      src: Himalayas,
+      children: [
+        { src: japan },
+        { src: SunFlowers },
+        // ... more child images
+      ],
+    },
+    {
+      src: SunFlowers,
+      children: [
+        { src: Vikendi },
+        { src: Vally },
+        // ... more child images
+      ],
+    },
+    {
+      src: Ganesh,
+      children: [
+        { src: Ranges },
+        { src: Temple },
+        // ... more child images
+      ],
+    },
+    {
+      src: Ranges,
+      children: [
+        { src: Mountains },
+        { src: Krishna },
+        // ... more child images
+      ],
+    },
+    {
+      src: Forest,
+      children: [
+        { src: Mountains },
+        { src: Krishna },
+        // ... more child images
+      ],
+    },
+    {
+      src: City,
+      children: [
+        { src: Shiva },
+        { src: Krishna },
+        // ... more child images
+      ],
+    },
+    {
+      src: japan,
+      children: [
+        { src: Mountains },
+        { src: SunFlowers },
+        // ... more child images
+      ],
+    }
   ];
 
 return (
